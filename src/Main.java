9561d37c4704085d371dee57a03d8f5ba125c1d0
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-19 11:27:35 +0530

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Scanner sc = new Scanner(System.in);
+        int s=0;
+        int e=1;
+        int n,sum=0;
+        n= sc.nextInt();
+        System.out.print(s+" "+e);
+        for(int i=0;i<n;i++){
+            sum =s+e;
+            s=e;
+            e=sum;
+            System.out.print(" "+sum+" ");
+
+        }
+        }
 }
