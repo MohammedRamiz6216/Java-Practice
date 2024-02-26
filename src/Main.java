88c9821440844c91fabdbb9af7ae60b0cfe9d169
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-26 11:45:14 +0530

    Add files

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,84 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int s=0;
-        int e=1;
-        int n,sum=0;
-        n= sc.nextInt();
-        System.out.print(s+" "+e);
-        for(int i=0;i<n;i++){
-            sum =s+e;
-            s=e;
-            e=sum;
-            System.out.print(" "+sum+" ");
 
+    import java.util.*;
+    /* // Fibanocci numbers
+           int s=0;
+           int e=1;
+           int n,sum=0;
+           n= sc.nextInt();
+           System.out.print(s+" "+e);
+           for(int i=0;i<n;i++){
+               sum =s+e;
+               s=e;
+               e=sum;
+               System.out.print(" "+sum+" ");
+
+           }
+            // prime numbers from 1 to n
+           int n=sc.nextInt();
+
+           for(int i=2;i<n;i++){
+               if(i==2){
+                   System.out.print(i+" ");
+               }
+               else if(i%2!=0){
+                   System.out.print(" "+i);
+               }
+           }
+           */
+ /* //palindrome number
+       int n = sc.nextInt();
+       int x=0,r=0,temp;
+       temp=n;
+       while(n>0){
+
+          x= n%10;
+           r = (r*10)+x;
+          n=n/10;
+
+
+       }
+       if(temp==r){
+           System.out.println("Is a palindrome");
+       }
+       else{
+           System.out.println("Not a palindrome");
+       }
+         */
+ /* //factorial of a number
+        int n = sc.nextInt();
+        int x=1;
+        for(int i=1;i<=n;i++){
+           x=x*i;
         }
+        System.out.println(x);
+  */
+public class Main{
+        public static void main(String[] args) {
+            System.out.println("Hello World");
         }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
