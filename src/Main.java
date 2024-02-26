be5c1a6c6076cbedec267727a164091f7a47173b
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-26 12:41:57 +0530

    Add files

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 
@@ -26,8 +27,8 @@ import java.util.Scanner;
                    System.out.print(" "+i);
                }
            }
-           */
- /* //palindrome number
+
+  //palindrome number
        int n = sc.nextInt();
        int x=0,r=0,temp;
        temp=n;
@@ -45,18 +46,138 @@ import java.util.Scanner;
        else{
            System.out.println("Not a palindrome");
        }
-         */
- /* //factorial of a number
+
+ //factorial of a number
         int n = sc.nextInt();
         int x=1;
         for(int i=1;i<=n;i++){
            x=x*i;
         }
         System.out.println(x);
+
+   //1st question
+            System.out.println("Hello World");
+
+
+  //2nd question
+            Scanner sc = new Scanner(System.in);
+            int a;
+            a=sc.nextInt();
+            System.out.println(a);
+
+
+      //3rd question
+          for(String t: args){
+              System.out.println(t);
+
+
+
+      //4th question
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter an integer");
+            int a;
+            a=sc.nextInt();
+            System.out.println("You have entered an integer");
+            System.out.println("Enter a string");
+            String s;
+            s= sc.next();
+            System.out.println("You have entered a string");
+            System.out.println("Enter a float");
+            float b;
+            b = sc.nextFloat();
+            System.out.println("You have entered an float");
+
+
+
+  //5th question
+            Scanner sc = new Scanner(System.in);
+            float f;
+            f= sc.nextInt();
+            f = ((f-32)*5)/9;
+            System.out.println(f);
+
+
+
+
+  //6th question
+            Scanner sc = new Scanner(System.in);
+            int x,y,temp;
+            x= sc.nextInt();
+            y=sc.nextInt();
+            temp = x;
+            x=y;
+            y=temp;
+            System.out.println("After swapping\nx="+x+"\ny="+y);
+
+
+
+ //7th question
+            Scanner sc = new Scanner(System.in);
+            int x,y;
+            x= sc.nextInt();
+            y=sc.nextInt();
+            x=x+y;
+            y=x-y;
+            x=x-y;
+            System.out.println("After swapping\nx="+x+"\ny="+y);
+
+
+
+  //8th question
+            Scanner sc = new Scanner(System.in);
+           String num1;
+           String num2;
+           num1 = sc.nextLine();
+           num2 = sc.nextLine();
+           BigInteger A = new BigInteger(num1);
+           BigInteger B = new BigInteger(num2);
+           BigInteger sum;
+           sum = A.add(B);
+           System.out.println(sum);
+
+
+ //9th question
+            Scanner sc = new Scanner(System.in);
+            int x;
+            int y;
+            int z;
+            x=sc.nextInt();
+            y=sc.nextInt();
+            z=sc.nextInt();
+            if(x>y && x>z){
+                System.out.println("x is the largest number");
+            }else if(y>x && y>z ){
+                System.out.println("y is the largest number");
+            }else if(z>x && z>y){
+                System.out.println("z is the largest number");
+            }else{
+                System.out.println("numbers are not different");
+            }
+
+
+
+
+//10th question
+           boolean teaching =false;
+           if(teaching){
+               System.out.println("good teacher");
+           }else{
+               System.out.println("why are you here?");
+           }
+
+
   */
 public class Main{
         public static void main(String[] args) {
-            System.out.println("Hello World");
+            //10th question
+
+
+
+
+
+
+
+
         }
     }
 
